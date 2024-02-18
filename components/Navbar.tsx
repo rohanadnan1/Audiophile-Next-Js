d63004fef18a34cdc8853aec0094b979acceb963
Rohan Adnan <bc230430189ruh@vu.edu.pk>
2024-02-18 18:50:55 +0500

    cart quantity show/hide added

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,11 +2,13 @@ import { Button, Container } from "@mui/material";
 import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
+import { useSelector } from "react-redux";
 import Cart from "./Cart";
 
 
 const Navbar = () => {
   const [showCart, setShowCart] = useState(false);
+  const cart = useSelector((state: any) => state.cart.cart);
   return (
     <div style={{backgroundColor: '#191919'}}>
       <Container
@@ -60,6 +62,24 @@ const Navbar = () => {
             width={30}
             height={30}
           />
+          {cart.length > 0 && (
+            <span
+              style={{
+                backgroundColor: "red",
+                color: "white",
+                borderRadius: "50%",
+                height: "1.5rem",
+                width: "1.5rem",
+                padding: "",
+                position: "relative",
+                top: "-1.3rem",
+                left: "-0.2rem",
+              }}
+            >
+              {cart.length}
+            </span>
+          
+          )}
         </Button>
         {showCart && <Cart />}
       </Container>
